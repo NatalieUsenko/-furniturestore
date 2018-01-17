d54c1454763bf37fee0fde2b5744cd2ea4b371ef
NatalieUsenko <usenkonatalie@gmail.com>
2018-01-17 17:18:10 +0200

    add fonts and bootstrap

--- wp-content/themes/grey/index.php
+++ wp-content/themes/grey/index.php
@@ -1,6 +1,4 @@
-<?php
-get_header(); ?>
-
+<?php get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -16,18 +14,11 @@ get_header(); ?>
 			// Start the loop.
 			while ( have_posts() ) : the_post();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
 				get_template_part( 'content', get_post_format() );
 
 			// End the loop.
 			endwhile;
 
-
-		// If no content, include the "No posts found" template.
 		else :
 			get_template_part( 'content', 'none' );
 
@@ -37,4 +28,4 @@ get_header(); ?>
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
 
-<?php //get_footer(); ?>
+<?php get_footer(); ?>
