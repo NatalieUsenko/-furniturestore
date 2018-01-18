ce71082ca4c21a03f99743b7759dc604cee4a2d1
NatalieUsenko <usenkonatalie@gmail.com>
2018-01-18 13:27:17 +0200

    add google API key

--- wp-content/themes/grey/functions.php
+++ wp-content/themes/grey/functions.php
@@ -28,6 +28,8 @@ function grey_scripts() {
 	wp_enqueue_style( 'grey-style', get_stylesheet_uri() );
     wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css' );
     wp_enqueue_style( 'font-style', 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' );
+    wp_register_script('googlemaps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAe_0rVt1WvsSNi8WGYeBWbUYqp3oiiubg',null,null,true);
+    wp_enqueue_script('googlemaps');
 	wp_enqueue_script( 'grey-script', get_template_directory_uri() . '/js/functions.js', array( 'jquery' ), '20180118', true );
     wp_enqueue_script( 'bootstrap-script', get_template_directory_uri() . '/js/bootstrap.min.js', true );
 }
