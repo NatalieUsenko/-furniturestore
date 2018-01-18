c65753e2c4d7d750159bdbedafb87acf987a7492
NatalieUsenko <usenkonatalie@gmail.com>
2018-01-18 15:47:33 +0200

    footer to bottom

--- wp-content/themes/grey/header.php
+++ wp-content/themes/grey/header.php
@@ -12,20 +12,18 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 
-	<div id="sidebar" class="sidebar">
-		<header id="masthead" class="site-header" role="banner">
-			<div class="site-branding">
-				<?php
+    <div id="content" class="site-content">
 
-					if ( is_front_page() && is_home() ) : ?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php else : ?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php endif;
-				?>
-			</div><!-- .site-branding -->
-		</header><!-- .site-header -->
+        <header id="masthead" class="site-header" role="banner">
+            <div class="site-branding">
+                <?php
+                    if ( is_front_page() && is_home() ) : ?>
+                        <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                    <?php else : ?>
+                        <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                    <?php endif;
+                ?>
+            </div><!-- .site-branding -->
+        </header><!-- .site-header -->
 
-	</div><!-- .sidebar -->
 
-	<div id="content" class="site-content">
