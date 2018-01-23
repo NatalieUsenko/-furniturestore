1e11ea4f078f06dd0cd77e73bc814d04f84ad120
NatalieUsenko <usenkonatalie@gmail.com>
2018-01-23 16:55:10 +0200

    header

--- wp-content/themes/grey/header.php
+++ wp-content/themes/grey/header.php
@@ -15,17 +15,18 @@
     <div id="content" class="site-content">
 
         <header id="masthead" class="site-header" role="banner">
-            <div class="site-branding">
-                <?php
-                $custom_logo_id = get_theme_mod( 'custom_logo' );
-                $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
-                echo $image[0];
-                    if ( is_front_page() && is_home() ) : ?>
-                        <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-                    <?php else : ?>
-                        <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-                    <?php endif;
-                ?>
+            <div class="container-fluid">
+                <div class="row" style="border: 1px solid blueviolet;">
+                    <div class="logo">
+                    <?php
+                    if ( function_exists( 'the_custom_logo' ) ) {
+                        the_custom_logo();
+                    } else {
+                        echo '<img src="'.get_template_directory_uri().'/img/top-logo.png" />';
+                    }
+                    ?>
+                    </div>
+                </div>
             </div><!-- .site-branding -->
         </header><!-- .site-header -->
 
