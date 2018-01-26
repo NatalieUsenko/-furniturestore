66b26fa751091466fce9264793e1e1c5c3c2c390
NatalieUsenko <usenkonatalie@gmail.com>
2018-01-26 16:58:05 +0200

    contacts tpl

--- wp-content/themes/grey/parts/form.php
+++ wp-content/themes/grey/parts/form.php
@@ -4,17 +4,17 @@
     <div class="row">
     <div class="col-md-6">
         <div class="form-group">
-            <input id="form_name" name="form_name" type="text" placeholder="Имя" class="form-control">
+            <input id="form_name" name="form_name" type="text" placeholder="Имя" class="form-control" required="required">
         </div>
     </div>
     <div class="col-md-6">
         <div class="form-group">
-            <input id="form_name" name="form_email" type="text" placeholder="E-mail" class="form-control">
+            <input id="form_name" name="form_email" type="text" placeholder="E-mail" class="form-control" required="required">
         </div>
     </div>
     <div class="col-md-12">
         <div class="form-group">
-            <textarea class="form-control" id="form_message" name="form_message" placeholder="Сообщение"></textarea>
+            <textarea class="form-control" id="form_message" name="form_message" placeholder="Сообщение" rows="4" required="required"></textarea>
         </div>
     </div>
     <div class="col-md-6">
