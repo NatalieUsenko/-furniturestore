b9608e552aed2e1f7b5192025dafcbb22dedcb81
NatalieUsenko <usenkonatalie@gmail.com>
2018-02-03 11:18:45 +0200

    contacts tpl

--- wp-content/themes/grey/js/functions.js
+++ wp-content/themes/grey/js/functions.js
@@ -1,9 +1,11 @@
 ( function( $ ) {
     $(document).ready(function() {
         $('.not-fixed').on('mouseenter', function () {
+            console.log('mouseenter');
             $(this).addClass('fixed').removeClass('not-fixed');
         });
         $('.fixed').on('mouseleave', function () {
+            console.log('mouseleave');
             $(this).addClass('not-fixed').removeClass('fixed');
         });
     })
