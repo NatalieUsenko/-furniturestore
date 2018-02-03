335193a7a970ccf7a5c9c9921a3ad0f6dc5bcd27
NatalieUsenko <usenkonatalie@gmail.com>
2018-02-03 11:47:56 +0200

    header

--- wp-content/themes/grey/js/functions.js
+++ wp-content/themes/grey/js/functions.js
@@ -1,10 +1,7 @@
 jQuery(document).ready(function($) {
-    console.log('ready');
     $('#masthead').mouseenter(function () {
-        console.log('mouseenter');
         $(this).addClass('fixed').removeClass('not-fixed');
     }).mouseleave(function () {
-        console.log('mouseleave');
         $(this).addClass('not-fixed').removeClass('fixed');
     });
 })
