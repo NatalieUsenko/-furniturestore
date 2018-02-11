f0e741e4fd57403d1736cb751a8143049007beb8
NatalieUsenko <usenkonatalie@gmail.com>
2018-02-11 15:43:25 +0200

    Statti tpl

--- wp-content/themes/grey/statti_page_tpl.php
+++ wp-content/themes/grey/statti_page_tpl.php
@@ -4,6 +4,7 @@ Template Name: Статьи
 Template Post Type: page
 */
 get_header();
+$current_page_id = get_the_ID();
 $top_args = array(
     'posts_per_page' => 1,
     'post_status' => 'publish',
@@ -19,6 +20,7 @@ $top_args = array(
 $top_news = new WP_Query($top_args);
 while ( $top_news->have_posts() ) {
     $top_news->the_post();
+    $top_news_id = get_the_ID();
     $big_img = get_field('big_img', get_the_ID());
     $top_news_content = get_the_content();
 }
@@ -27,18 +29,19 @@ while ( $top_news->have_posts() ) {
     <div id="primary" class="content-area <?php echo $big_img?'top-news_img':'';?>" <?php echo $big_img?'style="background-image: url('.$big_img.');"':'';?>>
         <main id="main" class="site-main" role="main">
                 <div class="container-fluid container" >
-                    <h1 class="page-title"><?php echo esc_html( get_the_title() ); ?></h1>
+                    <h1 class="page-title"><?php echo esc_html( get_the_title($current_page_id) ); ?></h1>
                     <?php if ( have_posts() ) : ?>
 
                     <?php
                     if ( $top_news->have_posts() ) { ?>
 
                     <div class="col-md-5">
-                        <?php
-                        the_date('d.m.Y');
-                        the_title(); // выведем заголовок поста
-                        echo cutString( the_content(), 50);
-                        ?>
+                        <div class="top-news_dark">
+                            <div class="post-top_date"><?php get_the_date('d.m.Y', $top_news_id);?></div>
+                            <div class="post-top_title"><?php  get_the_title($top_news_id);?></div>
+                            <div class="post-top_expert"><?php echo cutString( $top_news_content, 50);?></div>
+                            <div class="post-top_more-link"><a href="<?php echo get_the_permalink($top_news_id);?>">Детальнее</a></div>
+                        </div>
                     </div>
                     <?php }?>
                     <?php endif;?>
