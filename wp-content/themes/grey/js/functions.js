f6c5a8ed25393afcd8bdbc981d246cabad155162
NatalieUsenko <usenkonatalie@gmail.com>
2018-02-11 17:09:21 +0200

    Statti tpl

--- wp-content/themes/grey/js/functions.js
+++ wp-content/themes/grey/js/functions.js
@@ -4,4 +4,22 @@ jQuery(document).ready(function($) {
     }).mouseleave(function () {
         $(this).addClass('not-fixed').removeClass('fixed');
     });
-})
+});
+
+jQuery(window).load(function ($) {
+    if ( $('.top-news_dark').length > 0 ){
+        var windowWidth = $(window).width();
+        var leftStart = $('h1').position().left;
+        $('.top-news_dark').css('padding-left', leftStart+'px');
+
+        var darkHeight = $('.top-news_dark').outerHeight();
+        var imgHeight = Math.ceil(515*windowWidth/(1125));
+        var diffHeight = 0;
+        diffHeight = Math.ceil(imgHeight - imgHeight - $('h1').position().top - $('h1').height());
+        if ( diffHeight > 0 ){
+            $('.top-news_dark').css('margin-top', diffHeight+'px');
+        }
+
+
+    }
+});
