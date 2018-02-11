11a2e625216bd5256adfd86df567b53592179eaa
NatalieUsenko <usenkonatalie@gmail.com>
2018-02-11 13:59:08 +0200

    Statti tpl

--- wp-content/themes/grey/statti_page_tpl.php
+++ wp-content/themes/grey/statti_page_tpl.php
@@ -20,27 +20,31 @@ $top_news = new WP_Query($top_args);
 ?>
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
-
-                <?php if ( have_posts() ) : ?>
-                    <?php
-                    while ( $top_news->have_posts() ) {
-                        $top_news->the_post();
-                        $big_img = get_field('big_img', get_the_ID());
-
-                        $top_news_content = get_the_content();
-                        ?>
-                    <div class="col-md-5">
+            <div class="container-fluid">
+                <div class="container">
+                    <h1 class="page-title"><?php echo esc_html( get_the_title() ); ?></h1>
+                    <?php if ( have_posts() ) : ?>
                         <?php
-                        the_date('d.m.Y');
-                        the_title(); // выведем заголовок поста
-                        echo cutString( the_content(), 50);
-                    }
-                    ?>
+                        while ( $top_news->have_posts() ) {
+                            $top_news->the_post();
+                            $big_img = get_field('big_img', get_the_ID());
+
+                            $top_news_content = get_the_content();
+                            ?>
+                        <div class="col-md-5">
+                            <?php
+                            the_date('d.m.Y');
+                            the_title(); // выведем заголовок поста
+                            echo cutString( the_content(), 50);
+                        }
+                        ?>
+                        </div>
                     </div>
-                    <div class="col-md-7 top-news_img" style="background-image: url(<?php echo $big_img;?>);"> </div>
-                <?php
-                endif;
-                ?>
+                        <div class="col-md-7 top-news_img" style="background-image: url(<?php echo $big_img;?>);"> </div>
+                    <?php
+                    endif;
+                    ?>
+            </div>
             <div class="container-fluid container">
                 <h1 class="page-title"><?php echo esc_html( get_the_title() ); ?></h1>
             </div>
