ea888bfe3536e8cc7c051a15c8fea859f0f66c29
NatalieUsenko <usenkonatalie@gmail.com>
2018-02-11 17:44:25 +0200

    Statti tpl

--- wp-content/themes/grey/statti_page_tpl.php
+++ wp-content/themes/grey/statti_page_tpl.php
@@ -5,6 +5,7 @@ Template Post Type: page
 */
 get_header();
 $current_page_id = get_the_ID();
+$top_news_id = 0;
 $top_args = array(
     'posts_per_page' => 1,
     'post_status' => 'publish',
@@ -24,6 +25,13 @@ while ( $top_news->have_posts() ) {
     $big_img = get_field('big_img', get_the_ID());
     $top_news_content = get_the_content();
 }
+$news_args = array(
+    'posts_per_page' => 4,
+    'post_status' => 'publish',
+    'order' => 'DESC',
+    'post__not_in' => $top_news_id
+);
+$list_news = new WP_Query($news_args);
 
 ?>
     <div id="primary" class="content-area <?php echo $big_img?'top-news_img':'';?>" <?php echo $big_img?'style="background-image: url('.$big_img.');"':'';?>>
@@ -42,6 +50,21 @@ while ( $top_news->have_posts() ) {
                     </div>
 
                     <?php }?>
+                     <div class="container-fluid container">
+                    <?php while ( $list_news->have_posts() ) {
+                        $list_news->the_post();?>
+                        <div class="col-md-6">
+                            <div class="post-list_img"><?php the_post_thumbnail('thumbnail');?></div>
+                            <div class="post-list_date"><?php echo get_the_date(d.m.Y);?></div>
+                            <div class="post-list_title"><?php get_the_title();?></div>
+                            <div class="post-list_expert">><?php echo cutString( get_the_content(), 120);?></div>
+                            <div class="post-list_link-more"><a href="<?php echo get_the_permalink();?>">Детальнее</a></div>
+                        </div>
+
+
+
+                    <?php }?>
+                    </div>
                 <?php endif;?>
 
 
