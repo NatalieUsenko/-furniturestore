2fc37e2044690f351b413960b7d103bff9b9d4e0
NatalieUsenko <usenkonatalie@gmail.com>
2018-02-11 14:13:15 +0200

    Statti tpl

--- wp-content/themes/grey/statti_page_tpl.php
+++ wp-content/themes/grey/statti_page_tpl.php
@@ -17,38 +17,32 @@ $top_args = array(
     ),
 );
 $top_news = new WP_Query($top_args);
+while ( $top_news->have_posts() ) {
+    $top_news->the_post();
+    $big_img = get_field('big_img', get_the_ID());
+    $top_news_content = get_the_content();
+}
+
 ?>
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
-
-                <div class="container-fluid container">
+                <div class="container-fluid container" <?php echo $big_img?'style="background-image: url('.$big_img.');"':'';?>>
                     <h1 class="page-title"><?php echo esc_html( get_the_title() ); ?></h1>
                     <?php if ( have_posts() ) : ?>
-                        <?php
-                        while ( $top_news->have_posts() ) {
-                            $top_news->the_post();
-                            $big_img = get_field('big_img', get_the_ID());
 
-                            $top_news_content = get_the_content();
-                            ?>
-                        <div class="col-md-5">
-                            <?php
-                            the_date('d.m.Y');
-                            the_title(); // выведем заголовок поста
-                            echo cutString( the_content(), 50);
-                        }
-                        ?>
-                        </div>
-                    </div>
-                        <div class="col-md-7 top-news_img" style="background-image: url(<?php echo $big_img;?>);"> </div>
                     <?php
-                    endif;
-                    ?>
+                    while ( $top_news->have_posts() ) { ?>
 
-            <div class="container-fluid container">
-                <h1 class="page-title"><?php echo esc_html( get_the_title() ); ?></h1>
-            </div>
-
+                    <div class="col-md-5">
+                        <?php
+                        the_date('d.m.Y');
+                        the_title(); // выведем заголовок поста
+                        echo cutString( the_content(), 50);
+                        ?>
+                    </div>
+                    <?php }?>
+                    <?php endif;?>
+                </div>
 
         </main><!-- .site-main -->
     </div><!-- .content-area -->
