57311fc46b78139071f91639fdfff5257a7f1a38
NatalieUsenko <usenkonatalie@gmail.com>
2018-02-18 16:42:26 +0200

    post inner

--- wp-content/themes/grey/post.php
+++ wp-content/themes/grey/post.php
@@ -4,11 +4,11 @@ get_header();
 <div id="primary" class="content-area <?php echo $big_img?'top-news_img':'';?>" <?php echo $big_img?'style="background-image: url('.$big_img.');"':'';?>>
     <main id="main" class="site-main" role="main">
         <div class="container-fluid container" >
-            <?php if ( have_posts() ) { ?>
+            <?php if ( have_posts() ) { while ( have_posts() ){ the_post();?>
                 <div class="post_date"><?php echo get_the_date('d.m.Y', $top_news_id);?></div>
                 <h1 class="post_title"><?php echo esc_html( get_the_title() ); ?></h1>
                 <div class="post_content"><?php echo  get_the_content();?></div>
-            <?php }?>
+            <?php }}?>
         </div>
 
     </main><!-- .site-main -->
