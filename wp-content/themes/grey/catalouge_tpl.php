9832794c4695e736ba37a881adb4f0d0925cb174
Natalie <usenkonatalie@gmail.om>
2018-03-01 00:13:36 +0200

    inner post

--- wp-content/themes/grey/catalouge_tpl.php
+++ wp-content/themes/grey/catalouge_tpl.php
@@ -7,14 +7,14 @@ get_header();
 $current_page_id = get_the_ID();
 $big_img = get_field('big_img');
 ?>
-<div id="primary" class="content-area <?php echo $big_img?'top-news_img':'';?>" <?php echo $big_img?'style="background-image: url('.$big_img.');"':'';?>>
+<div id="primary" class="content-area <?php echo $big_img?'top-catalogue_img':'';?>" <?php echo $big_img?'style="background-image: url('.$big_img.');"':'';?>>
 	<main id="main" class="site-main" role="main">
 	<div class="container-fluid container" >
 		<h1 class="page-title"><?php echo esc_html( get_the_title($current_page_id) ); ?></h1>
 	</div>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-	<div class="top-news_dark">
+	<div class="top-catalogue_dark ">
 		<div class="hidden-lg hidden-md"><img src="<?php echo $big_img?$big_img:'';?>"></div>
 		<div class="post-top_content"><?php echo get_the_content_with_formatting();?></div>
 		<div class="post-top_more-link text-uppercase"><a href="javascript:void(0);">Каталог мебели</a></div>
