26c95e8ade0ff23805cb09e55270c22015b027d3
Natalie <usenkonatalie@gmail.om>
2018-02-28 21:08:17 +0200

    inner post

--- wp-content/themes/grey/single.php
+++ wp-content/themes/grey/single.php
@@ -4,7 +4,6 @@ $image_url = get_field('big_img');
 if (!empty($image_url)){
     $image_id = get_image_id($image_url);
     $image_thumb = wp_get_attachment_image_src($image_id, 'medium');
-    echo $image_thumb[0];
 }
 
 ?>
@@ -12,7 +11,12 @@ if (!empty($image_url)){
     <main id="main" class="site-main" role="main">
         <div class="container-fluid container" >
             <?php if ( have_posts() ) { while ( have_posts() ){ the_post();?>
-                <div class="post_img"><?php echo $image_thumb[0]?'<img src="'.$image_thumb[0].'">"':'';?></div>
+                <div class="post_img"><?php echo $image_thumb[0]?'<img src="'.$image_thumb[0].'">':'';?>
+                    <div class="col-xs-6">
+                        Рассказать друзьм <a href="javascript:void(0);"><img src="img/fb-grey.png"></a>
+                    </div>
+                    <div class="col-xs-6 text-right"><a href="/stati"><< Все статьи</a></div>
+                </div>
                 <div class="post_date"><?php echo get_the_date('d.m.Y');?></div>
                 <h1 class="post_title"><?php echo esc_html( get_the_title() ); ?></h1>
                 <div class="post_content"><?php echo  get_the_content();?></div>
