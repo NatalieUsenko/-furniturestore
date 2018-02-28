aba77eb174b222f539178d0b42eb9d77465ca59e
Natalie <usenkonatalie@gmail.om>
2018-02-28 23:37:27 +0200

    inner post

--- wp-content/themes/grey/single.php
+++ wp-content/themes/grey/single.php
@@ -31,6 +31,7 @@ $recomend = get_field('recomended');
                     <div class="post_title mt-50">Рекомендуем прочитать</div>
                     <?php foreach ($recomend as $value){
 			            $categories = get_the_category();
+			            var_dump($categories);
 			            ?>
                         <div class="col-md-6 post-list">
                             <div class="post-list_img"><?php echo get_the_post_thumbnail($value,'thumbnail');?></div>
