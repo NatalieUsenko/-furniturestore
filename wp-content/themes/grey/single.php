c4180f68798371ba6ae7fa95c057f27da2b406fb
Natalie <usenkonatalie@gmail.om>
2018-02-28 21:41:27 +0200

    inner post

--- wp-content/themes/grey/single.php
+++ wp-content/themes/grey/single.php
@@ -9,6 +9,7 @@ if (!empty($image_url)){
 ?>
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
+        <h1 style="display: none;"><?php echo esc_html( get_the_title() ); ?></h1>
         <div class="container-fluid container" >
             <?php if ( have_posts() ) { while ( have_posts() ){ the_post();?>
 
@@ -21,9 +22,9 @@ if (!empty($image_url)){
                             <div class="col-xs-6 text-right no-padding-right as-table-cell"><a href="<?php echo esc_url(home_url());?>/stati"><< Все статьи</a></div>
                         </div>
                     </div>
-                    <p class="post_date"><?php echo get_the_date('d.m.Y');?></p>
-                    <p class="post_title"><?php echo esc_html( get_the_title() ); ?></p>
-                    <p class="post_content"><?php echo  get_the_content();?></p>
+                    <div class="post_date"><?php echo get_the_date('d.m.Y');?></div>
+                    <div class="post_title"><?php echo esc_html( get_the_title() ); ?></div>
+                    <div class="post_content"><?php echo  get_the_content();?></div>
                 </div>
             <?php }}?>
         </div>
