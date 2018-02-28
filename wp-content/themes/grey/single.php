bbda6f206363a355177f6344600da5242b613475
Natalie <usenkonatalie@gmail.om>
2018-02-28 22:23:10 +0200

    inner post

--- wp-content/themes/grey/single.php
+++ wp-content/themes/grey/single.php
@@ -7,7 +7,6 @@ if (!empty($image_url)){
     $image_thumb = wp_get_attachment_image_src($image_id, 'medium');
 }
 $recomended = get_field('recomended');
-var_dump($recomended);
 
 ?>
 <div id="primary" class="content-area">
@@ -15,7 +14,6 @@ var_dump($recomended);
         <h1 style="display: none;"><?php echo esc_html( get_the_title() ); ?></h1>
         <div class="container-fluid container" >
             <?php if ( have_posts() ) { while ( have_posts() ){ the_post();?>
-
                 <div>
                     <div class="post_img"><?php echo !empty($image_thumb[0])?'<img src="'.$image_thumb[0].'">':'';?>
                         <div class="as-table-row">
@@ -29,6 +27,20 @@ var_dump($recomended);
                     <div class="post_title"><?php echo esc_html( get_the_title() ); ?></div>
                     <div class="post_content"><?php echo get_the_content_with_formatting();;?></div>
                 </div>
+                <?php if ($recomended){?>
+                    <div class="post_title">Рекомендуем прочитать</div>
+                    <?php foreach ($recomended as $value){?>
+                        <div class="col-md-6 post-list">
+                            <div class="post-list_img"><?php echo get_the_post_thumbnail($value,'thumbnail');?></div>
+                            <div class="post-list_date"><?php echo get_the_date($value,'d.m.Y');?></div>
+                            <div class="post-list_title"><?php echo get_the_title($value);?></div>
+                            <div class="post-list_expert"><?php echo cutString( get_the_content($value), 120);?></div>
+                            <div class="post-list_link-more"><a href="<?php echo get_the_permalink($value);?>">Детальнее</a></div>
+                        </div>
+                    <?php }?>
+                <?php }
+
+                ?>
             <?php }}?>
         </div>
 
