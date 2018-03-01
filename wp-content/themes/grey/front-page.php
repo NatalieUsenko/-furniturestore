8647e6b0832db8dfef88b931ad7d6fef929e587d
NatalieUsenko <usenkonatalie@gmail.com>
2018-03-01 19:48:16 +0200

    front page tpl

--- wp-content/themes/grey/front-page.php
+++ wp-content/themes/grey/front-page.php
@@ -51,6 +51,10 @@ $page_query = new WP_Query($page_args);
             ?>
         </div>
         <div class="slider"></div>
+        <div class="grey-line">
+            <a href="javascript:void(0);" data-to="catalogue" class="go-to">Каталог мебели</a>
+            <div id="slider-content"></div>
+        </div>
         <div class="clearfix"></div>
     </div>
     <div id="catalogue">
