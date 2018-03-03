74643156c7192784a038b01b6d635a9ca303ebfb
NatalieUsenko <usenkonatalie@gmail.com>
2018-03-03 18:14:41 +0200

    front page tpl

--- wp-content/themes/grey/front-page.php
+++ wp-content/themes/grey/front-page.php
@@ -54,8 +54,10 @@ $page_query = new WP_Query($page_args);
             <?php echo do_shortcode('[owl-carousel category="main-slider" singleItem="true" items="1" navigation="false" pagination="true" lazyLoad="true" slideSpeed="900" paginationSpeed="400"]');?>
         </div>
         <div class="grey-line">
+            <a id="owl-prew" href="javascript:void(0);"></a>
             <a href="javascript:void(0);" data-to="catalogue" class="go-to">Каталог мебели</a>
             <div id="slider-content"></div>
+            <a id="owl-next" href="javascript:void(0);"></a>
         </div>
         <div class="clearfix"></div>
     </div>
