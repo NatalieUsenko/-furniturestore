124089bfe01eb4df6b1ad8d63923c101dfaa4dd5
NatalieUsenko <usenkonatalie@gmail.com>
2018-03-04 20:30:32 +0200

    css

--- wp-content/themes/grey/js/functions.js
+++ wp-content/themes/grey/js/functions.js
@@ -62,8 +62,8 @@ jQuery(function($){
 
     $('.go-to').on('click', function(e) {
         e.preventDefault();
-        var to = $(this).attr('href');
-        $('html, body').animate({ scrollTop: $("+to+").offset().top }, 500);
+        var target = $(this.getAttribute('href'));
+        $('html, body').animate({ scrollTop: target.offset().top }, 500);
     });
 
     $(window).load(function () {
