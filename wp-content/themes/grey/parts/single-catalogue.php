bd8d7cc8ad13eb656f89f3ee56cd3bd0e2ec2114
NatalieUsenko <usenkonatalie@gmail.com>
2018-03-07 16:55:47 +0200

    single tpl

--- wp-content/themes/grey/parts/single-catalogue.php
+++ wp-content/themes/grey/parts/single-catalogue.php
@@ -1,10 +1,10 @@
 <?php
 $image_thumb = [];
 $image_url_left = get_field('big_img_left');
-
+$image_url_right = get_field('big_img_right');
 ?>
     <h1 style="display: none;"><?php echo esc_html( get_the_title() ); ?></h1>
-    <div class="container-fluid container" >
+    <div class="container-fluid" >
         <div class="page-title"><?php echo esc_html( get_the_title() ); ?></div>
 <?php if ( have_posts() ) {
     while (have_posts()) {
@@ -13,24 +13,33 @@ $image_url_left = get_field('big_img_left');
         $parts = explode('</p>', $orig_content);
 
         ?>
-        <div>
-            <div class="top-news_dark">
+        <div class="row">
+            <div id="right-img">
+                <?php if (!empty($image_url_right)){
+                    echo '<img src="'.$image_url_right.'" />';
+                }
+                ?>
+            </div>
+            <div id="#left-text" class="catalogue__inner_dark">
                 <?php if (!empty($parts)){
                     echo array_shift($parts)."</p>";
                 }
                 ?>
             </div>
-            <div id="#left-img_catalogue">
+        </div>
+        <div class="clearfix"></div>
+        <div class="row">
+            <div id="left-img">
                 <?php if (!empty($image_url_left)){
                     echo '<img src="'.$image_url_left.'" />';
                 }
                 ?>
             </div>
-            <div class="top-news_dark">
+            <div id="#right-text"  class="catalogue__inner_dark">
                 <?php if (!empty($parts)){
                     $last = array_pop($parts);
-                    $findme   = '';
-                    $pos = strpos($last, $findme);
+                    $find   = '';
+                    $pos = strpos($last, $find);
                     if ($pos === false) {
                         array_push($parts, $last);
                         $last = '';
--- wp-content/themes/grey/single.php
+++ wp-content/themes/grey/single.php
@@ -1,9 +1,9 @@
 <?php
 get_header();
 $categories = get_the_category();
-$image_url_right = get_field('big_img_right');
+
 ?>
-<div id="primary" class="content-area <?php echo $image_url_right?'top-news_img':'';?>" <?php echo $image_url_right?'style="background-image: url('.$image_url_right.');"':'';?>>
+<div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
     <?php if ( !empty($categories) ) {
         if ($categories[0]->term_id==1){
