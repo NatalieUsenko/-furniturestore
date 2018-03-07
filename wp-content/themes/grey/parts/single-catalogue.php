a062de70d7625889464abe4a6ae22ee8fcc1b9b8
NatalieUsenko <usenkonatalie@gmail.com>
2018-03-07 14:19:52 +0200

    single tpl

--- wp-content/themes/grey/parts/single-catalogue.php
+++ wp-content/themes/grey/parts/single-catalogue.php
@@ -1,4 +1,7 @@
 <?php
+$image_thumb = [];
+$image_url_left = get_field('big_img_left');
+
 ?>
     <h1 style="display: none;"><?php echo esc_html( get_the_title() ); ?></h1>
     <div class="container-fluid container" >
