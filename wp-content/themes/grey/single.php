93dc42095f05c436c734ad1e19e189ff2dd78672
NatalieUsenko <usenkonatalie@gmail.com>
2018-03-07 11:44:03 +0200

    single tpl

--- wp-content/themes/grey/single.php
+++ wp-content/themes/grey/single.php
@@ -1,50 +1,17 @@
 <?php
 get_header();
-$image_thumb = [];
-$image_url = get_field('big_img');
-if (!empty($image_url)){
-    $image_id = get_image_id($image_url);
-    $image_thumb = wp_get_attachment_image_src($image_id, 'medium');
-}
-$recomend = get_field('recomended');
-
+$categories = get_the_category();
 ?>
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
-        <h1 style="display: none;"><?php echo esc_html( get_the_title() ); ?></h1>
-        <div class="container-fluid container" >
-            <?php if ( have_posts() ) { while ( have_posts() ){ the_post();?>
-                <div>
-                    <div class="post_img"><?php echo !empty($image_thumb[0])?'<img src="'.$image_thumb[0].'">':'';?>
-                        <div class="as-table-row">
-                            <div class="col-xs-6 no-padding-left as-table-cell">
-                                Рассказать друзьм <a href="javascript:void(0);"><img src="<?php echo get_template_directory_uri();?>/img/fb-grey.png"></a>
-                            </div>
-                            <div class="col-xs-6 text-right no-padding-right as-table-cell"><a href="<?php echo esc_url(home_url());?>/stati"><< Все статьи</a></div>
-                        </div>
-                    </div>
-                    <div class="post_date"><?php echo get_the_date('d.m.Y');?></div>
-                    <div class="post_title"><?php echo esc_html( get_the_title() ); ?></div>
-                    <div class="post_content"><?php echo get_the_content_with_formatting();?></div>
-                </div>
-                <?php if ($recomend){?>
-                    <div class="post_title mt-50">Рекомендуем прочитать</div>
-                    <?php foreach ($recomend as $value){
-			            $categories = get_the_category($value);
-			            ?>
-                        <div class="col-md-6 post-list">
-                            <div class="post-list_img"><?php echo get_the_post_thumbnail($value,'thumbnail');?></div>
-                            <div class="post-list_date"><?php echo ($categories[0]->term_id==1)?get_the_date('d.m.Y', $value):$categories[0]->name;?></div>
-                            <div class="post-list_title"><?php echo get_the_title($value);?></div>
-                            <div class="post-list_expert"><?php echo cutString( get_the_content($value), 120);?></div>
-                            <div class="post-list_link-more"><a href="<?php echo get_the_permalink($value);?>">Детальнее</a></div>
-                        </div>
-                    <?php }?>
-                <?php }
+    <?php if ( !empty($categories) ) {
+        if ($categories[0]->term_id==1){
+            include_once('parts/single-news.php');
+        } else {
 
-                ?>
-            <?php }}?>
-        </div>
+        }
+    }
+    ?>
 
     </main><!-- .site-main -->
 </div><!-- .content-area -->
