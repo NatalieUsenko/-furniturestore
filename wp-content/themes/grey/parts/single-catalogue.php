944e7b2d8df3a638992a68c9daf0c8426845e401
NatalieUsenko <usenkonatalie@gmail.com>
2018-03-07 14:24:02 +0200

    single tpl

--- wp-content/themes/grey/parts/single-catalogue.php
+++ wp-content/themes/grey/parts/single-catalogue.php
@@ -5,9 +5,13 @@ $image_url_left = get_field('big_img_left');
 ?>
     <h1 style="display: none;"><?php echo esc_html( get_the_title() ); ?></h1>
     <div class="container-fluid container" >
+        <div class="page-title"><?php echo esc_html( get_the_title($current_page_id) ); ?></div>
 <?php if ( have_posts() ) {
     while (have_posts()) {
-        the_post(); ?>
+        the_post();
+        $orig_content = get_the_content_with_formatting();
+        var_dump($orig_content);
+        ?>
         <div>
 
         </div>
