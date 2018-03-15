98b2df04e18fd0b9f97aa7ad11a9fb478f689919
Natasha <usenkonatalie@gmail.com>
2018-03-15 13:10:38 +0200

    Designers fix

--- wp-content/themes/grey/statti_page_tpl.php
+++ wp-content/themes/grey/statti_page_tpl.php
@@ -68,7 +68,7 @@ $wp_query = new WP_Query($news_args);
                             <div class="post-list_img"><?php the_post_thumbnail('thumbnail');?></div>
                             <div class="post-list_date"><?php echo get_the_date('d.m.Y');?></div>
                             <div class="post-list_title"><?php echo get_the_title();?></div>
-                            <div class="post-list_expert"><?php echo cutString( get_the_content(), 120);?></div>
+                            <div class="post-list_expert"><?php echo cutString( strip_tags(get_the_content()), 120);?></div>
                             <div class="post-list_link-more"><a href="<?php echo get_the_permalink();?>">Подробнее</a></div>
                         </div>
                     <?php
