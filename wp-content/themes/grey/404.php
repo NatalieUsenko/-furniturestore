4293c8e107099fe96f7c5039a83d9f0f66ed9add
Natasha <usenkonatalie@gmail.com>
2018-05-02 09:28:48 +0300

    Add 404

---
+++ wp-content/themes/grey/404.php
@@ -0,0 +1,20 @@
+<?php
+get_header();
+?>
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <table width="100%" border="0">
+                <tr>
+                    <td width="50%">
+                        <div style="font-size: 450%; padding: 25px;">404</div>
+                    </td>
+                    <td>
+                        <div style="font-size: 150%;">Страница не найдена... </div>
+                        <div style="font-size: 115%;">или была перемещена по другому адресу </div>
+                    </td>
+                </tr>
+            </table>
+        </main><!-- .site-main -->
+    </div><!-- .content-area -->
+<?php
+get_footer();
