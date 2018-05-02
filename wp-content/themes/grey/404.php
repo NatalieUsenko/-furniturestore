76b679c69c400919b09199ae5953a5e2b7e89b7e
Natasha <usenkonatalie@gmail.com>
2018-05-02 09:33:12 +0300

    Add 404

--- wp-content/themes/grey/404.php
+++ wp-content/themes/grey/404.php
@@ -2,13 +2,13 @@
 get_header();
 ?>
     <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
+        <main id="main" class="site-main" role="main" style="padding-top: 50px;padding-bottom: 50px;">
             <table width="100%" border="0">
                 <tr>
-                    <td width="50%">
+                    <td width="50%" valign="middle" align="center">
                         <div style="font-size: 450%; padding: 25px;">404</div>
                     </td>
-                    <td>
+                    <td valign="middle">
                         <div style="font-size: 150%;">Страница не найдена... </div>
                         <div style="font-size: 115%;">или была перемещена по другому адресу </div>
                     </td>
